b45dd6e65f15847895e69eb4f2d584ac7ab9050c
Mitja Trampus <mtrampus@twitter.com>
2015-09-03 18:58:40 -0700

    translate: handle newlines correctly

--- translate.sh
+++ translate.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 # access translate.google.com from terminal
 
-help='translate <text> [[<source language>] <target language>]
+help='USAGE 1:
+translate.sh <text> [[<source language>] <target language>]
 if target missing, use DEFAULT_TARGET_LANG
-if source missing, use auto'
+if source missing, use auto
+
+USAGE 2:
+cat myfile | translate.sh [...]'
 
 # adjust to taste
 DEFAULT_TARGET_LANG=en
 TMP=/tmp/google_translate.out
 
-# Use stdin as input, if no cmd-line args are given. Limit to 10KB.
-text="${1:-$(cat | head -c10000)}"
-# Encode newlines
-text="$(echo "$text" | awk 1 ORS=' %%% ')"
-echo "TEXT: $text"
-echo "TRANS"
-
+# Parse arguments
 if [[ $1 = -h || $1 = --help ]]
 then
     echo "$help"
@@ -33,12 +31,23 @@ else
     target="$DEFAULT_TARGET_LANG"
 fi
 
-curl -s -i --user-agent "" -d "ie=UTF-8" -d "hl=en" -d "sl=$source" -d "tl=$target" --data-urlencode "text=$text" https://translate.google.com/m > $TMP
+# Querying Google
+translate_line() {
+  text=${1:0:10000}  # Limit size to 10k
+  curl -s -i --user-agent "" -d "ie=UTF-8" -d "hl=en" -d "sl=$source" -d "tl=$target" --data-urlencode "text=$text" https://translate.google.com/m > $TMP
 
-# after redirect (get both sets of headers) use last version of encoding:
-encoding=$(cat "$TMP" | grep -o -E 'charset=[^"]+' | tail -n1 | cut -d'=' -f2)
+  # after redirect (get both sets of headers) use last version of encoding:
+  encoding=$(cat "$TMP" | grep -o -E 'charset=[^"]+' | tail -n1 | cut -d'=' -f2)
 
-ans=$(cat "$TMP" | iconv -f $encoding | grep -o -E 'class="t0">.*?</div>' | cut -c12- ); 
-# Decode newlines
-echo "${ans%</div>}" #| sed -E 's/%%%/\'$'\n''/g'
+  ans=$(cat "$TMP" | iconv -f $encoding | grep -o -E 'class="t0">.*?</div>' | cut -c12- ); 
+  echo "${ans%</div>}"
+}
+
+# Use stdin as input, if no cmd-line args are given. Limit to 10KB.
+if [[ -z $1 ]]; then
+  while read -r line; do translate_line "$line"; done
+else
+  translate_line "$*"
+fi
+
 exit
