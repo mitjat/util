c8790a7435fd258ef4eec56545ac71ecfff8f848
Mitja T <mitjat@oasislabs.com>
2020-04-01 12:32:45 +0200

    Add GitHub/Oasis specific git tools

---
+++ add_changelog.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Adds a changelog entry for the current branch, assuming the branch corresponds to a GitHub PR.
+# Coded to the specifics of Oasis/Towncrier changelog collection setup:
+#  - Creates a .changelog/... file
+#  - Adds it to a new commit
+#  - Pushes the commit to origin
+
+set -euo pipefail
+
+# Determine PR number
+branch="$(git rev-parse --abbrev-ref HEAD)"
+pr="$(hub pr list -h ${branch} -f '%I')"
+
+# Check for existing changelog entries
+old_changelog="$(find .changelog/ -name "${pr}"'*.md' -print -quit)"
+if [[ "$old_changelog" != "" ]]; then
+  echo "NOTE: $old_changelog already exists. Feel free to Ctrl-C out of this."
+  echo ""
+fi
+
+# Determine type of change
+select changelog_type in breaking feature bugfix doc internal trivial; do
+  break
+done
+
+# Edit the file
+path=".changelog/${pr}.${changelog_type}.md"
+if [[ "$changelog_type" == "trivial" ]]; then
+  touch $path
+else
+  [[ -s "$path" ]] && hub pr list -h "$branch" -f '%t%n%n%b%n' >"$path"  # Seed changelog with PR description
+  vi "$path"
+  [[ -s "$path" ]] || { 
+    rm -f "$path"; echo "Empty changelog file; aborting."; break;
+  }
+fi
+
+# Autoformat
+prettier --write --print-width 78 --prose-wrap always "$path" || {
+  echo "Did not autoformat the file; install prettier with 'npm i -g prettier'"
+}
+
+# Add to new commit
+git status --porcelain | grep -E '^[^ ?]' -q && {
+  # ^ "git status --porcelain" will list *staged* files with a status character in the first column of the line.
+  #   Untracked files will have a status of "?".
+  echo "Will not auto-commit; staging area is not empty"
+  exit 1
+}
+git add -f "$path"
+git commit -m 'Add changelog entry'
+git push origin
---
+++ new_pr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Creates a new GitHub PR out of this branch.
+
+set -euo pipefail
+
+pr="$(hub pr list -h $(git branch-name) -f '%I')"
+
+if [[ "$pr" != "" ]]; then
+ echo "This branch already has a PR: $pr"
+ exit 1;
+fi;
+
+git push --set-upstream origin $(git branch-name)
+hub pull-request -e
+add_changelog.sh
+
+# Open PR in browser
+which xdg-open >/dev/null 2>/dev/null &&
+  xdg-open "$(hub pr list -h $(git branch-name) -f %U)"
